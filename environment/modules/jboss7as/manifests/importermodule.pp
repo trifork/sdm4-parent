89f1f0ccd5b54a1c936762f5925e7774f39fc961
Jakob Færch <jrf@trifork.com>
2012-09-17 10:04:26 +0200

    Skift sdm-core til nsp-util 1.0.9 for at få JBoss 7-venlig opsætning af slalog. Skift importermoduler til at køre mod opdateret (snapshot)-version af core, så de får rettelsen.

--- environment/modules/jboss7as/manifests/importermodule.pp
+++ environment/modules/jboss7as/manifests/importermodule.pp
@@ -7,6 +7,8 @@ define jboss7as::importermodule( $importername = '') {
         $modulename = $importername
       }
 
+	$module_main_dir="/pack/jboss/modules/sdm4/config/${modulename}/main"
+
 	File {
 		ensure => present,
     	owner => "jboss",
@@ -14,33 +16,34 @@ define jboss7as::importermodule( $importername = '') {
 	}
 
 	# NSP-Util Slalog configuration
-	file {"/pack/jboss/domain/slalog-conf/nspslalog-${modulename}.properties":
+	file {"${module_main_dir}/nspslalog-${modulename}.properties":
 		content => template("jboss7as/nspslalog-importer.properties"),
 	}
 
-	file {"/pack/jboss/domain/slalog-conf/log4j-nspslalog-${modulename}.properties":
+	file {"${module_main_dir}/log4j-nspslalog.properties":
 		content => template("jboss7as/log4j-nspslalog-importer.properties"),
 	}
 
-    file {"/pack/jboss/modules/sdm4/config/${modulename}/main/rowset.jar":
+	# support for Spring use of com.sun.rowset.CachedRowsetImpl, only needed because the tests run on OpenJDK which does not include this class
+    file {"${module_main_dir}/rowset.jar":
         ensure => present,
         source => "puppet:///modules/jboss7as/rowset.jar",
-        require => File["/pack/jboss/modules/sdm4/config/${modulename}/main"]
+        require => File["${module_main_dir}"]
     }
 
 	# JBoss module
-   	file {["/pack/jboss/modules/sdm4/config/${modulename}", "/pack/jboss/modules/sdm4/config/${modulename}/main"]:
+   	file {["${module_main_dir}/..", "${module_main_dir}"]:
         ensure => directory,
         require => File["/pack/jboss"],
     }
 
-	file {"/pack/jboss/modules/sdm4/config/${modulename}/main/log4j.properties":
+	file {"${module_main_dir}/log4j.properties":
 		content => template("jboss7as/log4j-importer.properties"),
-		require => File["/pack/jboss/modules/sdm4/config/${modulename}/main"],
+		require => File["${module_main_dir}"],
 	}
 
-	file {"/pack/jboss/modules/sdm4/config/${modulename}/main/module.xml":
+	file {"${module_main_dir}/module.xml":
 		content => template("jboss7as/sdm-importer-module.xml"),
-		require => File["/pack/jboss/modules/sdm4/config/${modulename}/main"],
+		require => File["${module_main_dir}"],
 	}
 }
